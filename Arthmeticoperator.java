1a99ce023f93cac2f0c59dddd05d65275432bf2a
UmeshMehta1 <ukmehta1245@gmail.com>
2024-02-02 08:03:06 +0545

    swap

---
+++ Arthmeticoperator.java
@@ -0,0 +1,15 @@
+class Arthmeticoperator{
+
+   public static void main(String arg[]){
+
+       byte a = 9,b=7;
+      
+       a= (byte)(a +b);
+       b= (byte)(a-b);
+       a= (byte)(a-b);
+       
+        System.out.println(a+" "+b);
+
+     }
+
+}
